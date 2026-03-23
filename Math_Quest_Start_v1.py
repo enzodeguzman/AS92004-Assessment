86436a981aa9ac9c42880f08828381fe93d6db63
Enzo de Guzman <enzo.deguzman@papamoacollege.school.nz>
2026-03-24 09:29:20 +1300

    Add files via upload

---
+++ Math_Quest_Start_v1.py
@@ -0,0 +1,19 @@
+# Functions
+
+
+# Print Introduction / Instructions
+print("""                  [---➗➕✖️ Welcome to the Math Quiz! ✖️➕➗---]
+=====================================================================
+=== This quiz will test your basic facts knowledge over multiple questions. ===
+===       Try your best to get every question correct, and have fun!         ===
+=====================================================================
+""")
+
+# Asks for users name and stores response to use for a more personalised test.
+name = input("Before we begin, Please enter your name: ")
+print("Hello, " + {name} + ". Would you like to see the instructions?")
+
+want_instruction = input()
+
+
+
